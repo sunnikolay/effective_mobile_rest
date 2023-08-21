2e171d6ab3b8cd99cea6135dafcf02a84aa6a262
Nikolai Stiazhkin <stns@tu.komus.net>
2023-08-21 11:40:08 +0300

    first commit

---
+++ src/main/java/com/nstiazhkin/smapi/SmApiApp.java
@@ -0,0 +1,13 @@
+package com.nstiazhkin.smapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmApiApp {
+    
+    public static void main( String[] args ) {
+        SpringApplication.run( SmApiApp.class, args );
+    }
+    
+}
